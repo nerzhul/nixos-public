ae6c9ad2fe28aa3b9d128409a8305e4cdcf039fb
Loic Blot <loic.blot@unix-experience.fr>
2024-03-14 09:59:42 +0100

    fix(cloud-init): disable rsyslog module, there is no rsyslog

--- kubernetes-node/default.nix
+++ kubernetes-node/default.nix
@@ -8,4 +8,17 @@
   services.containerd.enable = true;
   services.kubelet.enable = true;
   services.cloud-init.enable = true;
+
+  services.cloud-init.settings.cloud_init_modules = [
+    "migrator"
+    "seed_random"
+    "bootcmd"
+    "write-files"
+    "growpart"
+    "resizefs"
+    "update_hostname"
+    "resolv_conf"
+    "ca-certs"
+    "users-groups"
+  ];
 }
