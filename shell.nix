472d349b4be2b2913c8f259f1ce3d7f314ddbf45
Loic Blot <loic.blot@unix-experience.fr>
2024-03-14 08:54:44 +0100

    devel: add envrc & shell.nix

---
+++ shell.nix
@@ -0,0 +1,12 @@
+{ pkgs ? import <nixpkgs> { },
+}:
+
+pkgs.mkShell {
+  LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+  env.LANG = "C.UTF-8";
+  env.LC_ALL = "C.UTF-8";
+
+  packages = [];
+  shellHook = ''
+  '';
+}
