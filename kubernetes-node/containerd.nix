c7e79f937d6b5eb2a69143c13f0a736a5b0b8afc
Loïc Blot <nerzhul@users.noreply.github.com>
2024-04-09 13:42:20 +0200

    fix: containerd 1.17.5

--- kubernetes-node/containerd.nix
+++ kubernetes-node/containerd.nix
@@ -3,13 +3,13 @@ let
   cfg = config.services.containerd;
   containerd = with pkgs; buildGoModule rec {
     pname = "containerd";
-    version = "1.7.14";
+    version = "1.7.15";
 
     src = fetchFromGitHub {
       owner = "containerd";
       repo = "containerd";
       rev = "v${version}";
-      hash = "sha256-okTz2UCF5LxOdtLDBy1pN2to6WHi+I0jtR67sn7Qrbk=";
+      hash = "sha256-qLrPLGxsUmgEscrhyl+1rJ0k7c9ibKnpMpsJPD4xDZU=";
     };
 
     vendorHash = null;
