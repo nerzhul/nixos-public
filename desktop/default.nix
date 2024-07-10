244e9fc28cc5277333bd907cd2e6c90487dc8d70
Loïc BLOT <loic.blot@unix-experience.fr>
2024-07-10 16:18:02 +0200

    desktop: add more default configs

--- desktop/default.nix
+++ desktop/default.nix
@@ -5,5 +5,14 @@
     ./desktopEnv.nix
     ./keyring.nix
   ];
+
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    curl
+    git
+  ];
+
+  programs.direnv.enable = true;
+  system.copySystemConfiguration = true;
 }
 
