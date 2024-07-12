69b7b1b6f71ed020247a137d14e7b3bcda7f8621
Loïc BLOT <loic.blot@unix-experience.fr>
2024-07-12 14:31:02 +0200

    fix(desktop): add user rootless to docker group

--- desktop/docker.nix
+++ desktop/docker.nix
@@ -2,6 +2,7 @@
 {
   virtualisation.docker.enable = true;
   virtualisation.docker.storageDriver = "zfs";
+  users.extraGroups.docker.members = [ "nrz" ];
   virtualisation.docker.rootless = {
     enable = true;
     setSocketVariable = true;
