d91d9383ab42375199c960d00da4934919f2aa7d
Loïc BLOT <loic.blot@unix-experience.fr>
2024-07-12 11:51:23 +0200

    fix(desktop): add missing docker

--- desktop/default.nix
+++ desktop/default.nix
@@ -10,6 +10,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     curl
     git
+    docker
   ];
 
   programs.direnv.enable = true;
