e3e245f6857ce79b5bdfef84e07fbbbda87d5d16
Loïc BLOT <loic.blot@unix-experience.fr>
2024-08-26 09:25:58 +0200

    fix(desktop): add tailscale package by default

--- desktop/default.nix
+++ desktop/default.nix
@@ -11,6 +11,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     curl
     git
+    tailscale
   ];
 
   programs.direnv.enable = true;
