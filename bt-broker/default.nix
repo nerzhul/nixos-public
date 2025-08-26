d44317e9891e6960abec2aa766a8fd005b79ae88
Loic Blot <loic.blot@unix-experience.fr>
2025-08-26 09:13:04 +0200

    feat: add bt-broker simple config

---
+++ bt-broker/default.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, ... }:
+{
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+        General = {
+        Experimental = true; # Show battery charge of Bluetooth devices
+        };
+    };
+  };
+
+  services.pipewire = {
+    enable = true; # if not already enabled
+  };
+}
