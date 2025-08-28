3be4050e85c37a428447ca38d45da8a9bcc85ca3
Loic Blot <loic.blot@unix-experience.fr>
2025-08-28 13:13:40 +0200

    try to fix bluetooth firmware for RPI 5 custom kernel

--- bt-broker/default.nix
+++ bt-broker/default.nix
@@ -14,6 +14,21 @@
     enable = true; # if not already enabled
   };
 
+  nixpkgs.overlays = [
+    (self: super: {
+      linux-firmware-uncompressed = super.linux-firmware.overrideAttrs (old: {
+        postInstall = (old.postInstall or "") + ''
+          echo "Decompressing rtl8761bu_fw.bin.zst..."
+          mkdir -p $out/lib/firmware/rtl_bt
+          zstd -d $out/lib/firmware/rtl_bt/rtl8761bu_fw.bin.zst \
+            -o $out/lib/firmware/rtl_bt/rtl8761bu_fw.bin
+        '';
+      });
+    })
+  ];
+
+  hardware.firmware = [ pkgs.linux-firmware-uncompressed ];
+
   hardware.enableAllFirmware = true;
   hardware.enableRedistributableFirmware = true;
   nixpkgs.config.allowUnfree = true;
