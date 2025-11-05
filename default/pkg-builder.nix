bea5df1739023d1073177103a24818351f6aab76
Loic Blot <loic.blot@unix-experience.fr>
2025-11-05 20:25:13 +0100

    add a comment for pkg-builer key

--- default/pkg-builder.nix
+++ default/pkg-builder.nix
@@ -11,6 +11,11 @@ with lib;
     '';
   };
 
+  # To generate keys for nix-builder cache:
+  # sudo mkdir -p /etc/nix/keys
+  # sudo nix key generate-secret --key-name mycache > /etc/nix/keys/mycache-priv.pem
+  # sudo nix key convert-secret-to-public /etc/nix/keys/mycache-priv.pem > /etc/nix/keys/mycache-pub.pem
+
   config = mkIf config.nixBuilder {
     nix.settings.secret-key-files = [
       "/etc/nix/keys/mycache-priv.pem"
