0df1bad7386983b1b7c420064697c97de918f7ea
Loic Blot <loic.blot@unix-experience.fr>
2026-05-07 08:06:21 +0200

    disable root on ssh & make journald only in memory with low footprint

--- default/default.nix
+++ default/default.nix
@@ -4,8 +4,13 @@
   # Enable the OpenSSH daemon.
   services.openssh = {
     enable = true;
-    settings.PermitRootLogin = "yes";
+    settings.PermitRootLogin = "no";
   };
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  services.journald.extraConfig = ''
+    Storage=volatile
+    RuntimeMaxUse=128M
+  '';
 }
