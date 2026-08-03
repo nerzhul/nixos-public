a32df0553d72d6f8117833848801bff496f248a4
Loic Blot <loic.blot@unix-experience.fr>
2026-08-03 11:02:13 +0200

    fix(rpi-5): proper kernel build

--- rpi-5/default.nix
+++ rpi-5/default.nix
@@ -1,21 +1,66 @@
-{config, lib, pkgs, ... }:
+{ config, pkgs, lib, ... }:
+
 let
-  # Must be synced with kernel.nix
-  nix_rpi5_pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/054533f5c84884e3186a722c07cbb4a6c1a2af19.tar.gz") {};
-  linux_rpi5 = nix_rpi5_pkgs.callPackage ./kernel.nix {
-    kernelPatches = with nix_rpi5_pkgs.kernelPatches; [
+  modDirVersion = "6.1.63";
+  tag = "stable_20231123";
+
+  # Définition de la dérivation du noyau Linux RPi5
+  linux_rpi5 = (pkgs.buildLinux {
+    version = "${modDirVersion}-${tag}";
+    inherit modDirVersion;
+
+    src = pkgs.fetchFromGitHub {
+      owner = "raspberrypi";
+      repo = "linux";
+      rev = tag;
+      hash = "sha256-4Rc57y70LmRFwDnOD4rHoHGmfxD9zYEAwYm9Wvyb3no=";
+    };
+
+    # See https://gitlab.com/vriska/nix-rpi5/-/blob/main/linux-rpi.nix?ref_type=heads
+    # specific to RPI5
+    defconfig = "bcm2712_defconfig";
+
+    kernelPatches = with pkgs.kernelPatches; [
       bridge_stp_helper
       request_key_helper
     ];
-    ignoreConfigErrors = true;
-  };
 
-  linuxPackages_rpi5 = nix_rpi5_pkgs.linuxPackagesFor linux_rpi5;
-in {
-  boot.kernelPackages = linuxPackages_rpi5;
-  environment.systemPackages = with nix_rpi5_pkgs; [
-    raspberrypifw
-    raspberrypiWirelessFirmware
-    raspberrypi-eeprom
-  ];
+    features = {
+      efiBootStub = false;
+    };
+
+    extraMeta = {
+      platforms = with lib.platforms; arm ++ aarch64;
+      hydraPlatforms = [ "aarch64-linux" ];
+    };
+  }).overrideAttrs (oldAttrs: {
+    postConfigure = ''
+      # Empêche la présence de '-v7' ou autre suffixe qui altèrerait le modDirVersion
+      sed -i $buildRoot/.config -e 's/^CONFIG_LOCALVERSION=.*/CONFIG_LOCALVERSION=""/'
+      sed -i $buildRoot/include/config/auto.conf -e 's/^CONFIG_LOCALVERSION=.*/CONFIG_LOCALVERSION=""/'
+    '';
+
+    # Duplication des fichiers DTB pour assurer la compatibilité U-Boot
+    postFixup = ''
+      dtbDir=${if pkgs.stdenv.isAarch64 then "$out/dtbs/broadcom" else "$out/dtbs"}
+      rm -f $dtbDir/bcm283*.dtb
+      copyDTB() {
+        if [ -f "$dtbDir/$1" ]; then
+          cp -v "$dtbDir/$1" "$dtbDir/$2"
+        fi
+      }
+    '' + lib.optionalString (lib.elem pkgs.stdenv.hostPlatform.system [ "armv7l-linux" "aarch64-linux" ]) ''
+      copyDTB bcm2710-rpi-zero-2.dtb bcm2837-rpi-zero-2.dtb
+      copyDTB bcm2710-rpi-3-b.dtb bcm2837-rpi-3-b.dtb
+      copyDTB bcm2710-rpi-3-b-plus.dtb bcm2837-rpi-3-a-plus.dtb
+      copyDTB bcm2710-rpi-3-b-plus.dtb bcm2837-rpi-3-b-plus.dtb
+      copyDTB bcm2710-rpi-cm3.dtb bcm2837-rpi-cm3.dtb
+      copyDTB bcm2711-rpi-4-b.dtb bcm2838-rpi-4-b.dtb
+    '';
+  });
+
+in
+{
+  # Injection des paquets de noyau personnalisés dans le système
+  boot.kernelPackages = pkgs.linuxPackagesFor linux_rpi5;
 }
