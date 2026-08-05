2367cca97705b44b7dcc108f36b96d1e35861831
Loic Blot <loic.blot@unix-experience.fr>
2026-08-05 08:12:58 +0200

    disable cloud-init from kubernetes nodes

--- kubernetes-node/default.nix
+++ kubernetes-node/default.nix
@@ -12,19 +12,4 @@
 
   services.containerd.enable = true;
   services.kubelet.enable = true;
-  services.cloud-init.enable = true;
-  services.cloud-init.network.enable = false;
-
-  services.cloud-init.settings.cloud_init_modules = [
-    "migrator"
-    "seed_random"
-    "bootcmd"
-    "write-files"
-    "growpart"
-    "resizefs"
-    "update_hostname"
-    "resolv_conf"
-    "ca-certs"
-    "users-groups"
-  ];
 }
